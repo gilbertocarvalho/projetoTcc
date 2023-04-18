3e8732cf8ebe7e9b37b124b8fc2ed89f68d8c0e9
gilbertocarvalho <gilberto.sousa2013@gmail.com>
2023-04-17 22:52:21 -0300

    Alteração na classe livro com adição de autor e editora

--- sistemaTCC/backend/Livraria/src/main/java/edu/curso/Livraria/Livro.java
+++ sistemaTCC/backend/Livraria/src/main/java/edu/curso/Livraria/Livro.java
@@ -5,10 +5,12 @@ import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
-
-
-
+import java.util.List;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 @Entity    
 public class Livro{
 
@@ -22,6 +24,9 @@ public class Livro{
                      
                 private long quantidade;
 
+                private List<Autor> autores;
+
+                     private Editora editora;
 
 
 
@@ -34,8 +39,16 @@ public class Livro{
 		this.isbn = isbn;
 	}
 
-                   
-                
+             @ManyToMany
+          @JoinTable(name = "autorlivro", joinColumns = @JoinColumn(name="isbn"),inverseJoinColumns =@JoinColumn(name= "codigo"))
+ public List<Autor> getAutores(){
+
+           return autores;
+
+}
+        	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}        
 
 
 	public String getTitulo() {
@@ -63,7 +76,16 @@ public class Livro{
              public long getQuantidade() {
 		return quantidade;
 	}
-         
+                      @ManyToOne
+                       @JoinColumn(name="codigo")
+                 	public Editora getEditora() {
+		return editora;
+	}
+
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
+
 
 	
 }
